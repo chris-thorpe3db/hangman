f4b2f7473515a4a39b68106a056b2a291f1fd24b
Chris Thorpe <drummer1074spam@gmail.com>
2024-04-18 13:32:33 +0000

    Rework HangClient exception handling

--- HangClient.cs
+++ HangClient.cs
@@ -1,18 +1,22 @@
 ﻿using Hangman.Exceptions;
-
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Hangman {
     public class HangClient {
+
         public static async Task<string> GetWord(string? url) {
             if (url == null || url == "") {
                 url = "https://random-word-api.herokuapp.com/word?number=1";
             }
             HttpClient client = new HttpClient();
-            HttpResponseMessage response;
-            
-            response = await client.GetAsync(url);
+            HttpResponseMessage response = null;
+            try {
+                response = await client.GetAsync(url);
+            } catch (Exception) {
+                throw new BadImageFormatException("Expected HTTP 200 OK using URL " + url + " , got " + (int)response.StatusCode);
+            }
             int statusCode = (int)response.StatusCode;
             if (response.IsSuccessStatusCode) {
                 string json = await response.Content.ReadAsStringAsync();
