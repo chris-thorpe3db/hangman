b6bc06143c98c1a015b8eb5fd55162fab0bbed60
Chris Thorpe <drummer1074spam@gmail.com>
2024-04-18 15:17:30 +0000

    Add license notice to all files

--- HangClient.cs
+++ HangClient.cs
@@ -1,4 +1,21 @@
-﻿#nullable enable annotations
+﻿/*
+ * This file (HangClient.cs) is part of Hangman.
+ *
+ * Hangman is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hangman is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hangman. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#nullable enable annotations
 using Hangman.Exceptions;
 using System;
 using System.Net.Http;
